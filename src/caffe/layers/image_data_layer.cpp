c7079a0e91f309c626199018b1fc6b4f4b79b235
Takuya Narihira <dvbscb@gmail.com>
2015-01-12 15:19:43 -0800

    Random seed option to ImageDataLayer

--- src/caffe/layers/image_data_layer.cpp
+++ src/caffe/layers/image_data_layer.cpp
@@ -44,7 +44,10 @@ void ImageDataLayer<Dtype>::DataLayerSetUp(const vector<Blob<Dtype>*>& bottom,
   if (this->layer_param_.image_data_param().shuffle()) {
     // randomly shuffle data
     LOG(INFO) << "Shuffling data";
-    const unsigned int prefetch_rng_seed = caffe_rng_rand();
+    unsigned int prefetch_rng_seed = caffe_rng_rand();
+    if (this->layer_param_.image_data_param().has_rand_seed()) {
+      prefetch_rng_seed = this->layer_param_.image_data_param().rand_seed();
+    }
     prefetch_rng_.reset(new Caffe::RNG(prefetch_rng_seed));
     ShuffleImages();
   }
